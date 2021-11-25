4d832b9c692a15124fcca8d5688096254ee052db
wissmedia <wissmedia@gmail.com>
2021-11-25 18:33:46 +0800

    done

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  let Hello = process.env.HELLO;
+</script>
+
+<section class="hero is-danger is-fullheight">
+  <div class="hero-body">
+    <div class="">
+      <p class="title">
+        {Hello}!
+      </p>
+      <p class="subtitle">Svelte Bulma Template</p>
+    </div>
+  </div>
+</section>
+
+<style>
+</style>
